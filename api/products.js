d915c6211bcee1c6b589f23f63fb0c9a3109ada5
ezebuikay-ctrl <ezebuikay@gmail.com>
2026-06-28 19:54:20 +0100

    Add API handler for product listings

---
+++ api/products.js
@@ -0,0 +1,24 @@
+export default function handler(req, res) {
+  const products = [
+    { 
+      "id": 1, 
+      "name": "BEX Premium Rice", 
+      "price": "N45,000", 
+      "description": "High quality long grain rice", 
+      "imageUrl": "https://bexmarket-ng.vercel.app/images/rice.jpg" 
+    },
+    { 
+      "id": 2, 
+      "name": "Refined Vegetable Oil", 
+      "price": "N12,500", 
+      "description": "Pure vegetable oil", 
+      "imageUrl": "https://bexmarket-ng.vercel.app/images/oil.jpg" 
+    }
+  ];
+  
+  res.status(200).json({
+    "message": "Success",
+    "products": products
+  });
+}
+
